a1f335956a0981004a8346c8c619a897621e6654
RyantSanders <ryantsanders13@gmail.com>
2014-08-12 21:18:36 -0500

    Screencast
    adding more screen cast things into the rep

---
+++ String and Booleans variables/js/script.js
@@ -0,0 +1,10 @@
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 39; // Number variable
+
+var isStudent = true;
+
+
+var firstName = "Ryan";
+
+var phrase = 'I don\'t \n know you\'ll have to speak with the professor';
