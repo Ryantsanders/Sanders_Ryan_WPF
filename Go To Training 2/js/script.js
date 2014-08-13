28063b57a0b0b1ac244d636685e9c7702619a832
RyantSanders <ryantsanders13@gmail.com>
2014-08-13 16:26:35 -0500

    Go To Training
    Following along with the go to training

---
+++ Go To Training 2/js/script.js
@@ -0,0 +1,29 @@
+/*
+Ryan Sanders
+WPF 1418 Section 01
+Name of Assignment
+8-13-2014
+ */
+
+//alert("Testing 1,2,3")
+
+//Create a js file that will tell us how old we are
+
+///Ask the user what year they were born in
+//Create a variable to catch thier answer
+
+
+//Ask the user their name
+var name = prompt ("Please type in your name:");
+
+//Say hello to the user and let them know what we are doing
+//Alert the user
+alert("Welcome "+name+"! let's go ahead and figure out how old you are");
+
+
+
+
+var yearBorn = prompt ("what year were you born?");
+
+//Console.log out the response
+console.log(yearBorn);
