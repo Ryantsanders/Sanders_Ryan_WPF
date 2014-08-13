40484ae107020a4abb23d1c0cc0cc5226117e69c
RyantSanders <ryantsanders13@gmail.com>
2014-08-13 17:57:44 -0500

    Saving changes
    updating the changed made to my files

--- Expressions_Personal/js/Personal.js
+++ Expressions_Personal/js/Personal.js
@@ -8,3 +8,15 @@
 //alert("Test");
 
 //I am going to attempt to calculate how much water I will drink over the next 6months
+
+//Asking the user for their name to get everything started
+var name = prompt("Hello, we haven\'t met before could you please tell me your name");
+
+//alerting the user and greeting them as well as letting them know what is going on
+alert("Hello there "+name+", I am trying to figure out how much waster you drink every 6 months")
+
+
+
+var waterDrankDaily = prompt("How many glasses of  water do you drink on a daily basis?");
+console.log(waterDrankDaily);
+
