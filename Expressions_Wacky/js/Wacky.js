235b0b774b300442ac296d0ecbf704cdbcd4cd34
RyantSanders <ryantsanders13@gmail.com>
2014-08-14 00:30:46 -0500

    update
    update

--- Expressions_Wacky/js/Wacky.js
+++ Expressions_Wacky/js/Wacky.js
@@ -11,4 +11,15 @@ console.log(name);
 
 //alerting the user and greeting them as well as letting them know what is going on
 alert("Hello there "+name+", this time around we are going to go on a wacky adventure hope you're ready!");
+console.log("Hello there "+name+", this time around we are going to go on a wacky adventure hope you're ready!");
+
+//Going to attempt to make it so their choice in the prompt will become the variable
+
+var itemChoices = [IceCream, Syrup, Milk]; //0,1,2
+
+//alert going to ask them to make a choice based on the options
+alert("Hey there again "+name+" I have a few choices here for you I would like to pick from.");
+var itemPicked = prompt("Hiya "+name+" please make a choice out of the items listed.");
+
+
 
