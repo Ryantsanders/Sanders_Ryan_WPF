0790ca0d09f1e71ac6cf718ef344300326651f19
RyantSanders <ryantsanders13@gmail.com>
2014-08-20 17:59:17 -0500

    saved data
    saving

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -13,6 +13,19 @@ Name of Assignment
 var name  = prompt("Why hello there visitor from another dimension. Who am I? no need to worry about that right now might I instead suggest you tell me your name?");
  console.log(name);
 
+//validating the prompt
+ if(name === "") {
+     name = prompt("Please enter your name!");
+ }
+
+
+
+
+
+
+//just a simple alert
 alert("Alright "+ name +", the theme of today is movies!!");
 
 //going to ask about the users age then i'll prob make a ternary based on age with a movie selection or something of that sort
+
+var age = prompt("say "+ name +", would you mind letting me know your age so I can suggest some movies for you?");
