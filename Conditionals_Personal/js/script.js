0fadd4e642972d611679fa46c90235848119402c
RyantSanders <ryantsanders13@gmail.com>
2014-08-20 17:55:07 -0500

    updating
    committing update every time I save..and I save often

---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,18 @@
+
+ /*
+Ryan Sanders
+WPF 1418 Section 01
+Name of Assignment
+8-13-2014
+ */
+
+//alert("Testing 1,2,3")
+
+
+// going to do something about movies perhaps since it would be a good fit to use a ternary
+var name  = prompt("Why hello there visitor from another dimension. Who am I? no need to worry about that right now might I instead suggest you tell me your name?");
+ console.log(name);
+
+alert("Alright "+ name +", the theme of today is movies!!");
+
+//going to ask about the users age then i'll prob make a ternary based on age with a movie selection or something of that sort
---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,9 @@
+
+ /*
+Ryan Sanders
+WPF 1418 Section 01
+Name of Assignment
+8-13-2014
+ */
+
+alert("Testing 1,2,3")
