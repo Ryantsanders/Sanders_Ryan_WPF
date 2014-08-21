15e5102ba97a72d2f0826ec0cc9ea4cb5786f0d4
RyantSanders <ryantsanders13@gmail.com>
2014-08-20 19:16:57 -0500

    update
    update

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -74,7 +74,7 @@ movieGirl = (age < 13)? "My Little Pony Equestria Girls, Frozen,Smurfs 2": "The 
 if(gender === "boy" || "Boy"){
     console.log("Here you go "+ name +", a few movies I think you might enjoy! "+ movieBoy);
 }
-if(gender === "girl"){
+if(gender === "girl" || "Girl"){
     console.log("Here you go "+ name +", a few movies I think you might enjoy! "+ movieGirl);
 }
 
