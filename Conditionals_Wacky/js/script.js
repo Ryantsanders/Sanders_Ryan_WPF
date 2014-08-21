f0f208efba75a029b106c60dcf2f1fcdf0bb8538
RyantSanders <ryantsanders13@gmail.com>
2014-08-20 22:18:40 -0500

    update
    update

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,3 +7,15 @@ Name of Assignment
  */
 
 //alert("Testing 1,2,3")
+
+
+//this is my last one to do I think i'll just make it another short and simple one
+
+ var name  = prompt("Hello! nice to meet ya! id give you my name but it is classified informations I woulld still however love to know your name please?");
+
+
+ if(name === "") {
+     name = prompt("Wow! you are like the third person to try and skip town without giving me their name today!");
+
+ }
+ console.log(name);
