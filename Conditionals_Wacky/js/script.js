3d9dd005462d742f6c0364854437bed21a76011a
RyantSanders <ryantsanders13@gmail.com>
2014-08-20 22:47:24 -0500

    update
    update

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -57,5 +57,5 @@ if(place === ""){
  if(place === zoo){
      console.log("Nice choice"+ name +", I bet here we can see lots of animals like lions, tigers and bears OH MY!!!");
  }else if(place === seaWorld){
-     console.log("YES I love this place way to go "+ name +", we can really see some awesome animals here like sharks, whales and dolphins!! "+ movieGirl);
+     console.log("YES I love this place way to go "+ name +", we can really see some awesome animals here like sharks, whales and dolphins!! ");
  }
