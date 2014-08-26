c3c5350d3b6eb878ad5d8f0a1d89557d5e11c403
RyantSanders <ryantsanders13@gmail.com>
2014-08-26 18:49:09 -0500

    update
    update

---
+++ Loops/js/dowhileloop.js
@@ -0,0 +1,32 @@
+
+ /*
+Ryan Sanders
+WPF 1418 Section 01
+Name of Assignment
+8-13-2014
+ */
+
+//alert("Testing 1,2,3")
+
+var b = 10;
+
+while (b > 0){
+    console.log(b + "kegs on the wall");
+    b--;
+}
+
+
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+
+}
+
+while(c > 0);
+
+//for loop is shorter
+for (var i = 10; i > 0; i--){
+    console.log(i + " kegs on th wall")
+}
---
+++ Loops/js/script.js
@@ -0,0 +1,17 @@
+
+ /*
+Ryan Sanders
+WPF 1418 Section 01
+Name of Assignment
+8-13-2014
+ */
+
+//alert("Testing 1,2,3")
+
+
+var b = 50; //setups the index
+
+while ( b > 0){ //checks the condition
+    console.log(b + "kegs on the wall");
+    b--; //increments or decrements the index
+}
