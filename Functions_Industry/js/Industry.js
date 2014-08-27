c45dfbdacc758b2f99341bfd6adaa0f5d43cc4d7
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 17:05:31 -0500

    update
    update

--- Functions_Industry/js/Industry.js
+++ Functions_Industry/js/Industry.js
@@ -5,10 +5,20 @@
  8-27-2014
  */
 
+//I think i'll shoot for something about building a pc I know we are suppose to do something industry related but I ran out of ideas
+// and we do need computers in this industry
+
 
 //Asking the user for their name to get everything started
 var name = prompt("Hello, my name is Ryan nice to meet you, could you please tell me your name");
 console.log(name);
+
+alert("So we are going to build a computer get excited!");
+
+
+
+
+
 
 function calcArea(){
     var width = 20;
