d867448b62faa289c92125973a30757958f32205
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 16:56:41 -0500

    update
    update

--- Functions_Industry/js/Industry.js
+++ Functions_Industry/js/Industry.js
@@ -5,9 +5,14 @@
  8-27-2014
  */
 
-//First i'll start with some type of ternary
-
 
 //Asking the user for their name to get everything started
 var name = prompt("Hello, my name is Ryan nice to meet you, could you please tell me your name");
 console.log(name);
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
