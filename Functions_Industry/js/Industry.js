39e7d8e483ce4f20cc93a6b181fad60a9233a789
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 16:45:59 -0500

    update
    update

--- Functions_Industry/js/Industry.js
+++ Functions_Industry/js/Industry.js
@@ -4,3 +4,10 @@
  Name of Assignment
  8-27-2014
  */
+
+//First i'll start with some type of ternary
+
+
+//Asking the user for their name to get everything started
+var name = prompt("Hello, my name is Ryan nice to meet you, could you please tell me your name");
+console.log(name);
