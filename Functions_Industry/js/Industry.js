7e64586640883a7ce3f4e7014355cacf9ca2b45a
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 17:18:55 -0500

    update
    update

--- Functions_Industry/js/Industry.js
+++ Functions_Industry/js/Industry.js
@@ -5,13 +5,20 @@
  8-27-2014
  */
 
-//I think i'll shoot for something about building a pc I know we are suppose to do something industry related but I ran out of ideas
+//I think i'll shoot for something about pc parts I know we are suppose to do something industry related but I ran out of ideas
 // and we do need computers in this industry
 
 
 //Asking the user for their name to get everything started
 var name = prompt("Hello, my name is Ryan nice to meet you, could you please tell me your name");
+
+if(name === "") {
+    name = prompt("Excuse me! I really need you to tell me your name!!");
+
+}
 console.log(name);
+
+
 
 alert("So we are going to pick out some parts for a computer get excited!");
 
@@ -20,6 +27,9 @@ console.log(computerCpu);
 
 var computerGpu = prompt("hmm next up what type of GPU do you prefer Nvidia or Amd?");
 console.log(computerGpu);
+
+
+
 
 
 
