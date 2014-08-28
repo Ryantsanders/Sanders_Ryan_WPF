0f0d134f597ebaa81846f6bd8d2897e8334e9518
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 20:33:18 -0500

    update
    update

--- Functions_Wacky/js/Wacky.js
+++ Functions_Wacky/js/Wacky.js
@@ -7,3 +7,18 @@ Name of Assignment
  */
 
 //alert("Testing 1,2,3")
+
+
+ //added name prompt and to check for validation
+ var name  = prompt("Hi hi! how are you doing could you kindly tell me your name?");
+
+
+ if(name === "") {
+     name = prompt("Oh, come on tell me your name! I won't tell anyone!");
+
+ }
+ console.log(name);
+
+ //simple alert saying whats going on
+ alert("Alright! so do you like video games? I know I sure do lets talk about a fighting came first!");
+
