aae4774c26ee584e61573a95aeda18f73923a2b9
RyantSanders <ryantsanders13@gmail.com>
2014-08-27 19:17:02 -0500

    update
    update

--- Functions_Personal/js/Personal.js
+++ Functions_Personal/js/Personal.js
@@ -6,4 +6,17 @@ Name of Assignment
 8-13-2014
  */
 
-alert("Testing 1,2,3")
+//alert("Testing 1,2,3")
+
+ //added name prompt and to check for validation
+ var name  = prompt("Hi hi! how are you doing could you kindly tell me your name?");
+
+
+ if(name === "") {
+     name = prompt("Whoa you must be a person of mystery! however I think it would be a good idea if you can tell me your name!");
+
+ }
+ console.log(name);
+
+ //simple alert saying whats going on
+ alert("Alright! so you want to build your own web site i'll help you out with prices!");
